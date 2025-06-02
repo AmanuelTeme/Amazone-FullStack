e5a5c59aef100b33a87bc84fbc757794b5c82a4c
AmanuelTeme <amanuel.teme1@gmail.com>
2025-06-02 13:50:37 +0200

    add backend-url

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-abafb/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-abafb/us-central1/api",
+  baseURL: "https://api-63zt7rzjpa-uc.a.run.app",
 });
